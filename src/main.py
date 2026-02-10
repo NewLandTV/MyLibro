232619cde06303ff81ec97cd12c69495f68b901f
JkhTV <candzzang5283@gmail.com>
2026-02-10 16:41:43 +0900

    Add loop

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from library import Library
+import sys
 
 def main():
-    myLibro = Library()
-    myLibro.add_book("004", "Test", "Dr. K")
-    myLibro.show_books()
-    myLibro.find_book_by_index("1234")
+    try:
+        myLibro = Library()
+        myLibro.add_book("004", "Test", "Dr. K")
+        myLibro.show_books()
+        myLibro.find_book_by_index("1234")
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("'Ctrl + C' 감지, MyLibro 프로그램을 종료합니다.")
+        sys.exit(0)
     
 if __name__ == "__main__":
     main()
