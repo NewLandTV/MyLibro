a05b6c183c50c0f5a7e1e74fab1a70b6ace47b04
JkhTV <candzzang5283@gmail.com>
2026-02-11 22:21:17 +0900

    Feat edit num read

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from library import Library
+from library import Book, Library
 import os
 import sys
+from typing import List
 
 myLibro = Library()
+
+def confirm(prompt: str) -> bool:
+    while True:
+        agree = input(f"{prompt} (y/n): ")
+        if agree.upper() == "Y":
+            return True
+        elif agree.upper() == "N":
+            return False
 
 def show_command_list():
     print("=== 명령어 목록 ===")
@@ -13,6 +22,7 @@ def show_command_list():
     print("5. /load : 도서관 데이터를 불러옵니다.")
     print("6. /exit : 도서관 프로그램을 종료합니다. (저장 안 됨)")
     print("7. /cls : 화면을 지웁니다.")
+    print("8. /read : 특정 도서의 회독 수를 변경합니다.")
 
 def add():
     index = input("\t색인: ")
@@ -20,15 +30,17 @@ def add():
     author = input("\t저자: ")
     print(f"\t[{index}] 제목: {title}, 저자: {author}")
 
-    while True:
-        agree = input("\t새 책으로 추가하시겠습니까? (y/n): ")
-        if agree.upper() == "Y":
-            myLibro.add_book(index, title, author)
-            return
-        elif agree.upper() == "N":
-            return
+    if confirm("\t새 책으로 추가하시겠습니까?"):
+        myLibro.add_book(index, title, author)
         
-def find():
+def find(show_result=True) -> List[Book] | None:
+    """
+    CLI 기반 도서 간단한 도서 검색 명령입니다.
+    
+    :param show_result: 결과를 화면에 보여줄지 여부입니다.
+    :return: 찾은 도서 목록이 있으면 리스트로 반환하지만, 없으면 None을 반환합니다.
+    :rtype: List[Book] | None
+    """
     while True:
         print("\t검색 방법을 번호로 쓰시오.")
         print("\t1. 색인")
@@ -40,13 +52,14 @@ def find():
             method = int(method)
             break
 
+    books = []
     match method:
         case 1:
             idx = input("\t색인: ")
             books = myLibro.find_book_by_index(idx)
             if not books:
                 print(f"\t[{idx}] 도서를 찾을 수 없습니다.")
-            else:
+            elif show_result:
                 print(f"\t[{idx}] 찾은 도서: {len(books)}권")
                 for book in books:
                     print(f"\t제목: {book.title}\n\t저자: {book.author}\n\t추가된 날짜: {book.added_date}\n\t회독: {book.num_read}회")
@@ -55,7 +68,7 @@ def find():
             books = myLibro.find_book_by_title(title)
             if not books:
                 print(f"\t제목이 '{title}'인 도서를 찾을 수 없습니다.")
-            else:
+            elif show_result:
                 print(f"\t제목이 '{title}'인 도서: {len(books)}권")
                 for book in books:
                     print(f"\t제목: {book.title}\n\t저자: {book.author}\n\t추가된 날짜: {book.added_date}\n\t회독: {book.num_read}회")
@@ -64,18 +77,91 @@ def find():
             books = myLibro.find_book_by_author(author)
             if not books:
                 print(f"\t저자가 '{author}'인 도서을 찾을 수 없습니다.")
-            else:
+            elif show_result:
                 print(f"\t저자가 '{author}'인 도서: {len(books)}권")
                 for book in books:
                     print(f"\t제목: {book.title}\n\t저자: {book.author}\n\t추가된 날짜: {book.added_date}\n\t회독: {book.num_read}회")
+    return books if books else None
 
 def exit():
+    if confirm("\t정말로 도서관 프로그램을 종료하시겠습니까?"):
+        sys.exit(0)
+
+def read():
+    books = find(False)
+    if not books:
+        return
+    
+    for i in range(len(books)):
+        print(f"\t{i + 1}. [{books[i].index}] 제목: '{books[i].title}', 저자: '{books[i].author}', 회독: {books[i].num_read}회")
+    
     while True:
-        agree = input("\t정말로 도서관 프로그램을 종료하시겠습니까? (y/n): ")
-        if agree.upper() == "Y":
-            sys.exit(0)
-        elif agree.upper() == "N":
-            return
+        number = input("\t회독 수를 수정할 검색된 도서 중 번호를 쓰시오: ")
+
+        if number.isdigit() and 1 <= int(number) <= len(books):
+            number = int(number) - 1
+            break
+
+    print(f"\t'[{books[number].index}] {books[number].title}', 선택한 도서의 현재 회독 수: {books[number].num_read}")
+
+    while True:
+        print("\t회독 수 수정 방법을 번호로 쓰시오.")
+        print("\t1. 1회독 추가")
+        print("\t2. 1회독 감소")
+        print("\t3. N회독 추가")
+        print("\t4. N회독 감소")
+        print("\t5. N회독으로 설정")
+        print("\t6. 초기화 (0회독으로 설정)")
+        method = input("\t번호 입력: ")
+
+        if method.isdigit() and 1 <= int(method) <= 6:
+            method = int(method)
+            break
+        
+    match method:
+        case 1:
+            books[number].num_read += 1
+            print(f"\t회독 수를 1만큼 추가했습니다. (현재: {books[number].num_read}회독)")
+        case 2:
+            if books[number].num_read > 0:
+                books[number].num_read -= 1
+                print(f"\t회독 수를 1만큼 감소했습니다. (현재: {books[number].num_read}회독)")
+            else:
+                print(f"\t회독 수가 0이므로 감소할 수 없습니다.")
+        case 3:
+            while True:
+                n = input("\t추가할 회독 수 입력(N은 자연수): ")
+
+                if n.isdigit() and 1 <= int(n):
+                    n = int(n)
+                    break
+            books[number].num_read += n
+            print(f"\t회독 수를 {n}만큼 추가했습니다. (현재: {books[number].num_read}회독)")
+        case 4:
+            if books[number].num_read == 0:
+                print(f"\t회독 수가 0이므로 감소할 수 없습니다.")
+                return
+            
+            while True:
+                n = input(f"\t감소할 회독 수 입력(N은 {books[number].num_read} 이하인 자연수): ")
+
+                if n.isdigit() and 1 <= int(n) <= books[number].num_read:
+                    n = int(n)
+                    break
+            books[number].num_read -= n
+            print(f"\t회독 수를 {n}만큼 감소했습니다. (현재: {books[number].num_read}회독)")
+        case 5:
+            while True:
+                n = input("\t설정할 회독 수 입력(N은 음이 아닌 정수): ")
+
+                if n.isdigit() and 0 <= int(n):
+                    n = int(n)
+                    break
+            books[number].num_read = n
+            print(f"\t회독 수를 {n}(으)로 설정했습니다. (현재: {books[number].num_read}회독)")
+        case 6:
+            if confirm("\t정말로 회독 수를 초기화하시겠습니까? (되돌릴 수 없습니다.)"):
+                books[number].num_read = 0
 
 def main():
     try:
@@ -98,6 +184,8 @@ def main():
                     exit()
                 elif cmd == "/cls":
                     os.system("cls")
+                elif cmd == "/read":
+                    read()
     except KeyboardInterrupt:
         myLibro.save_data_to_local_file()
         print("'Ctrl + C' 감지, MyLibro 프로그램을 종료합니다.")
