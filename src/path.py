ac687fab5ac74b6048e61e181a7f56d90ec12596
JkhTV <candzzang5283@gmail.com>
2026-09-06 11:54:28 +0900

    Remove src_path

--- src/path.py
+++ src/path.py
@@ -1,11 +1,10 @@
 from os import path
 
-project_root = path.dirname(path.dirname(path.abspath(__file__)))
-src_dir = path.dirname(path.abspath(__file__))
-data_dir = path.join(project_root, "data")
-
 def join(root: str, target: str) -> str:
     return path.join(root, target)
+
+project_root = path.dirname(path.dirname(path.abspath(__file__)))
+data_dir = join(project_root, "data")
 
 def exists(pth: str):
     return path.exists(pth)
